0805e1c35b1c205450e402a98a023d390182d459
Jean C. Solís <jcsolisu2000@gmail.com>
2020-03-28 14:33:05 -0600

    Optimizacion de asignacion de parametros

--- src/SQLString.java
+++ src/SQLString.java
@@ -2,24 +2,12 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class SQLString {
 
-	class Parameter {
-		public String name;
-		public String partial;
-	}
-
 	// SQL Text
 	private String sqlText;
-
-	// internal SQL Text
-	private String internalText;
-
-	private List<Parameter> parameters;
 
 	private String defaultValue(Object obj, String value, String defaultValue) {
 		return (obj == null) ? defaultValue : value;
@@ -35,88 +23,12 @@ public class SQLString {
 		}
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		sqlText = reader.lines().collect(Collectors.joining("\n"));
+		sqlText = reader.lines().collect(Collectors.joining("\n")).toUpperCase();
 		reader.close();
-
-		extractParameters();
 	}
 
 	private void setParameter(String nombre, String valor) {
-		Boolean encontrado = false;
-		internalText = "";
-
-		for (int i = 0; i < parameters.size(); i++) {
-			if (parameters.get(i).name.equalsIgnoreCase(nombre)) {
-				internalText = internalText + parameters.get(i).partial + valor;
-				encontrado = true;
-			} else {
-				internalText += parameters.get(i).partial + "?";
-			}
-		}
-
-		if (!encontrado) {
-			throw new IllegalArgumentException(String.format("No existe el parametro [%s].", nombre));
-		}
-	}
-
-	private void extractParameters() {
-		parameters = new ArrayList<Parameter>();
-
-		int length = sqlText.length();
-		StringBuffer parsedQuery = new StringBuffer(length);
-
-		boolean inSingleQuote = false;
-		boolean inDoubleQuote = false;
-		boolean inSingleLineComment = false;
-		boolean inMultiLineComment = false;
-
-		for (int i = 0; i < length; i++) {
-			char c = sqlText.charAt(i);
-			Parameter parameter = null;
-
-			if (inSingleQuote) {
-				if (c == '\'') {
-					inSingleQuote = false;
-				}
-			} else if (inDoubleQuote) {
-				if (c == '"') {
-					inDoubleQuote = false;
-				}
-			} else if (inMultiLineComment) {
-				if (c == '*' && sqlText.charAt(i + 1) == '/') {
-					inMultiLineComment = false;
-				}
-			} else if (inSingleLineComment) {
-				if (c == '\n') {
-					inSingleLineComment = false;
-				}
-			} else if (c == '\'') {
-				inSingleQuote = true;
-			} else if (c == '"') {
-				inDoubleQuote = true;
-			} else if (c == '/' && sqlText.charAt(i + 1) == '*') {
-				inMultiLineComment = true;
-			} else if (c == '-' && sqlText.charAt(i + 1) == '-') {
-				inSingleLineComment = true;
-			} else if (c == ':' && i + 1 < length && Character.isJavaIdentifierStart(sqlText.charAt(i + 1))) {
-				int j = i + 2;
-				while (j < length && Character.isJavaIdentifierPart(sqlText.charAt(j))) {
-					j++;
-				}
-				String name = sqlText.substring(i + 1, j);
-				parameter = new Parameter();
-				parameter.name = name;
-				parameters.add(parameter);
-				c = '?';
-				i += name.length();
-			}
-			parsedQuery.append(c);
-		}
-
-		String[] textSplit = parsedQuery.toString().split("\\?");
-		for (int i = 0; i < parameters.size(); i++) {
-			parameters.get(i).partial = textSplit[i];
-		}
+		sqlText = sqlText.replaceAll(String.format(":%s(?=[^\\w])", nombre.trim().toUpperCase()), valor);
 	}
 
 	public void setDouble(String nombre, Double valor) {
@@ -144,13 +56,10 @@ public class SQLString {
 	}
 
 	public String toString() {
-		return internalText;
+		return sqlText;
 	}
 
 	public void setSqlText(String sqlText) {
 		this.sqlText = sqlText;
-
-		extractParameters();
 	}
-
 }
